92023c48008c3e3d03aa08df1ca85e56dfd005c2
SlaneDRV <125742851+SlaneDRV@users.noreply.github.com>
2024-06-02 16:27:32 +0200

    Probably working

---
+++ www/index.php
@@ -0,0 +1,19 @@
+<?php
+echo "Default page served by Nginx";
+echo "<br>Revutski Dzmitry";
+
+$host = 'mysql'; 
+$username = 'user'; 
+$password = 'password'; 
+$dbname = 'test_db';
+
+$mysqli = new mysqli($host, $username, $password, $dbname);
+
+if ($mysqli->connect_error) {
+    die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+}
+
+echo "Connected successfully to MySQL database using mysqli.";
+
+$mysqli->close();
+?>
